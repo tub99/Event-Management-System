cc2e673f2b1cc5bafdd1902a6040c88864c1400f
soumya.ghosh <ghoshsoumya001@gmail.com>
2019-01-07 22:45:12 +0530

    unnecessary codes removed

--- client/app/scripts/controllers/addEventsCtrl.js
+++ client/app/scripts/controllers/addEventsCtrl.js
@@ -1,7 +1,5 @@
 app.controller('addEventsCtrl', ['$scope', '$state', 'EventsService','APP_CONSTANTS', function (scope, state, EventsService,constants) {
     scope.eventAddStatus = false;
-    scope.showAlert = false;
-    scope.alertMessage = '';
 
     scope.addAnEvent = function () {
         
@@ -14,9 +12,5 @@ app.controller('addEventsCtrl', ['$scope', '$state', 'EventsService','APP_CONSTA
         });
     }
 
-    scope.displayStatus = function (bool) {
-        scope.showAlert = true;
-        scope.eventAddStatus = bool;
-        scope.alertMessage = bool ? constants.SUCCESS_MESSAGE : constants.ERROR_MESSAGE;
-    }
+  
 }]);
