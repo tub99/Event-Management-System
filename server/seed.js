9cf8da23f1bab0d9f15136da0ec0ed12f69a0092
soumya ghosh <soumya.ghosh@pearson.com>
2019-01-08 15:10:52 +0530

    seed url updated

--- server/seed.js
+++ server/seed.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"undefined": "localhost/EventManager",
-	"dev": "localhost/EventManager",
-	"prod": "localhost/EventManager"
+	"undefined": "mongodb://somevent:somevent1234@ds145302.mlab.com:45302/event-manager-db",
+	"dev": "mongodb://somevent:somevent1234@ds145302.mlab.com:45302/event-manager-db",
+	"prod": "mongodb://somevent:somevent1234@ds145302.mlab.com:45302/event-manager-db"
 }
